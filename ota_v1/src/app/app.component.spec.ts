73f35f693f30eed6401900ad38004df387a08bad
zvdas <rodriguezzude@gmail.com>
2022-05-23 23:40:55 +0530

    new folder structure

--- ota_v1/src/app/app.component.spec.ts
+++ ota_v1/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'onlineTestApp'`, () => {
+  it(`should have as title 'ota_v1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('onlineTestApp');
+    expect(app.title).toEqual('ota_v1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('onlineTestApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ota_v1 app is running!');
   });
 });
