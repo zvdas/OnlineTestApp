11bff0a866481e6a196c380dda25f5bde8518b11
zvdas <rodriguezzude@gmail.com>
2022-05-26 19:12:28 +0530

    classes created (Model)

--- ota_v2/src/app/classes/user/user.ts
+++ ota_v2/src/app/classes/user/user.ts
@@ -1,13 +1,15 @@
 export class User {
     name:string;
     email:string;
-    username:any;
-    password:any;
+    username:string;
+    password:string;
+    isAdmin:boolean;
 
-    constructor(name:string, email:string, username:any, password:any){
+    constructor(name:string, email:string, username:string, password:string, isAdmin:boolean){
         this.name = name;
         this.email = email;
         this.username = username;
         this.password = password;
+        this.isAdmin = isAdmin;
     }
 }
