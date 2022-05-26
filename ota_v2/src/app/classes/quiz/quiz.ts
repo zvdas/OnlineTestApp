8be3127ebfe7655c8b91361a1eedda07846daec6
zvdas <rodriguezzude@gmail.com>
2022-05-26 18:31:36 +0530

    classes created (Model)

--- ota_v2/src/app/classes/quiz/quiz.ts
+++ ota_v2/src/app/classes/quiz/quiz.ts
@@ -1,2 +1,15 @@
 export class Quiz {
+    qno:number;
+    question:string;
+    options:string[];
+    answer:string;
+    ansindex:number;
+
+    constructor(qno:number, question:string, options:string[], answer:string, ansindex:number){
+        this.qno = qno;
+        this.question = question;
+        this.options = options;
+        this.answer = answer;
+        this.ansindex = ansindex;
+    }
 }
--- ota_v2/src/app/classes/user/user.ts
+++ ota_v2/src/app/classes/user/user.ts
@@ -1,2 +1,13 @@
 export class User {
+    name:string;
+    email:string;
+    username:any;
+    password:any;
+
+    constructor(name:string, email:string, username:any, password:any){
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
 }
