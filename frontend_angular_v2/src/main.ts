fc41ee30bcda060c8cbc42a8ad8dc9a3ef68fe88
Jude Rodriguez <rodriguezzude@gmail.com>
2023-10-29 11:24:24 +0530

    bootstrap imported

--- frontend_angular_v2/src/main.ts
+++ frontend_angular_v2/src/main.ts
@@ -2,7 +2,9 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { environment } from './environments/environment.prod';
+
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 if (environment.production) {
   enableProdMode();
