1aaca14535ea492aeed24ade6ef70f750b806801
Jude Rodriguez <rodriguezzude@gmail.com>
2024-03-30 14:38:38 +0530

    logic updated

--- frontend_angular_v2/src/app/guards/auth/auth.guard.ts
+++ frontend_angular_v2/src/app/guards/auth/auth.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Injectable({
@@ -9,22 +10,20 @@ import { AuthService } from 'src/app/services/auth/auth.service';
 
 export class AuthGuard implements CanActivate {
 
-  constructor(private as: AuthService, private router: Router) {}
+  constructor(private afAuth: AngularFireAuth, private router: Router) { }
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    // const currentUser = JSON.parse(localStorage.getItem('user')!)['providerData'];
-    // const currentUser = JSON.parse(localStorage.getItem('user')!);
-    const currentUser = this.as.isLoggedIn;
-
-    if(!currentUser) {
-      window.alert('Access Denied. Login to access this page.');
-      // this.router.navigateByUrl('/auth/login')
-      // this.router.navigate(['/error', {errorCode: 401, errorTitle: 'Access Denied', errorDescription: 'Access Denied. Login to access this page.'}]);
-    }
-    return true;
+  canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.afAuth.authState.pipe(
+      map(user => {
+        if (user) {
+          return true;
+        } else {
+          // this.router.navigate(['/error', {errorCode: 401, errorTitle: 'Access Denied', errorDescription: 'Access Denied. Login to access this page.'}]);
+          this.router.navigateByUrl('/auth/login');
+          return false;
+        };
+      })
+    )
   }
-  
+
 }
