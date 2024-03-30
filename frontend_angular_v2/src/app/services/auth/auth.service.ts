34064a743ba22075b9ee9832d2823d827716c52c
Jude Rodriguez <rodriguezzude@gmail.com>
2024-03-30 15:15:21 +0530

    method added to get logged in user

--- frontend_angular_v2/src/app/services/auth/auth.service.ts
+++ frontend_angular_v2/src/app/services/auth/auth.service.ts
@@ -1,48 +1,46 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class AuthService {
+  authSubject = new BehaviorSubject(null);
 
-  constructor(private as: AngularFireAuth, private router: Router) {
-    as.authState.subscribe(user => {
-      user ? localStorage.setItem('user', JSON.stringify(user)) : localStorage.setItem('user', 'null');
-    })
-  }
-
-  isLoggedIn(): boolean {
-    // return !JSON.parse(localStorage.getItem('user')!) ? true : false;
-    return !JSON.parse(localStorage.getItem('user')!);
+  constructor(private afAuth: AngularFireAuth, private router: Router) {
   }
 
   registerNewUser(email: string, password: string) {
-    this.as
+    this.afAuth
       .createUserWithEmailAndPassword(email, password)
   }
 
   loginRegisteredUser(email: string, password: string) {
-    this.as
+    this.afAuth
       .signInWithEmailAndPassword(email, password)
       .then(creds => localStorage.setItem('user', JSON.stringify(creds.user)))
       .then(() => this.router.navigateByUrl('/quiz/master'));
   }
 
+  getLoggedInUser() {
+    return this.afAuth.currentUser;
+  }
+
   forgotPassword(resetEmail: string) {
-    this.as
+    this.afAuth
       .sendPasswordResetEmail(resetEmail);
   }
 
   verifyEmail(resetCode: string) {
-    this.as
+    this.afAuth
       .verifyPasswordResetCode(resetCode);
   }
 
   logOutUser() {
-    return this.as
+    return this.afAuth
       .signOut()
       .then(() => localStorage.removeItem('user'))
       .then(() => this.router.navigateByUrl('/auth/login'));
