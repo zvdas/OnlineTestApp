0f8e7ed2db7f9a067af28daf87471fdac7b4d8de
Jude Rodriguez <rodriguezzude@gmail.com>
2024-08-04 17:54:13 +0530

    update: realtime functionality for current date and time implemented

--- frontend_angular_v2/src/app/modules/layout/navbar/navbar.component.ts
+++ frontend_angular_v2/src/app/modules/layout/navbar/navbar.component.ts
@@ -1,8 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription, takeUntil } from 'rxjs';
 import { Auth } from 'src/app/models/auth';
 import { User } from 'src/app/models/user';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { DateTimeService } from 'src/app/services/date-time/date-time.service';
 
 @Component({
   selector: 'app-navbar',
@@ -10,17 +12,41 @@ import { AuthService } from 'src/app/services/auth/auth.service';
   styleUrls: ['./navbar.component.css']
 })
 
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
 
   @Input() isLoggedIn: boolean = false;
 
-  currentUser: any;
-  currentDate = new Date();
+  private subscriptions: Subscription[] = [];
 
-  constructor(private authService: AuthService, private router: Router) { }
+  currentUser: any;
+  currentDate: Date = new Date();
+
+  constructor(
+    private authService: AuthService,
+    private dateTimeService: DateTimeService,
+    private router: Router,
+  ) { }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
+  }
 
   ngOnInit(): void {
     this.currentUser = this.authService.getLoggedInUser();
+
+    const loginStateSub = this.authService.getLoginState().subscribe(res => {
+      if(res?.uid) {
+        this.isLoggedIn = true;
+      } else {
+        this.isLoggedIn = false;
+      };
+    });
+    this.subscriptions.push(loginStateSub);
+
+    const dateTimeSub = this.dateTimeService.currentDate$.subscribe(date => {
+      this.currentDate = date;
+    });
+    this.subscriptions.push(dateTimeSub);
   }
 
   Logout() {
